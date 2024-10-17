4dbb73c32d39254d7ae7005ad40dd8fdd4eaf88c
AnteArig <gabriele.ariganello@gmail.com>
2024-10-17 12:35:46 +0200

    home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { CocktailsComponent } from './cocktails/cocktails.component';
 
 const routes: Routes = [
+
+  {
+    path: '', redirectTo: '/home', pathMatch: 'full'
+  },
+
+  {
+    path: 'home', component: HomeComponent
+  },
+
   {
     path: 'contacts', component: ContactsComponent
   },
 
 
   {
-    path: 'cocktails', component: CocktailsComponent 
+    path: 'cocktails', component: CocktailsComponent
   }
 ];
 
